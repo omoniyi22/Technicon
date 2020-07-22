97d4fb5c2b061e3e23dfd27c93553576b9281e0c
oluwaseun <omoniyi22>
2020-07-22 18:01:14 +0100

    removal of some components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
 import React from "react";
-import "./App.css";
-import { NavBar } from "./components/navbar/navbar";
-import UserPageChatDashBoard from "./components/page-content/dashboard/user-chat-and-history";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { AboutPage } from "./components/page-content/about-page/about-page";
-import Dashboard from "./components/page-content/dashboards/index";
-import UserPaymentGateWay from "./components/page-content/dashboard/user-payment-methods";
-import LandingPage from "./components/page-content/landing-page/landing-page";
-import HamburgerMenu from "./components/hamburger-menu/hamburger-menu";
-/*
-Routes
-
-user-profile
-transacrion-history
-Dashboard
-new-tranasction
-payment-gateway
-thank-you
-edit-profile
-pickup-location
-reached-out
-oFfice-address
-invoice
-payment-method
-
-*/
+import Dashboard from "./components/Dashboard/index";
 
 
 class App extends React.Component {
@@ -32,17 +8,10 @@ class App extends React.Component {
     return (
       <>
         <Router>
-          <NavBar />
-          <HamburgerMenu />
           <Switch>
             <Route path="/dashboard" component={Dashboard} />
             <Route path="/new-transaction" component={Dashboard} />
             <Route path="/payment-gateway" component={Dashboard} />
-
-        
-            
-            <Route path="/about" component={AboutPage} />
-            <Route path="/"  component={LandingPage} />
           </Switch>
         </Router>
       </>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "antd/dist/antd.css";
-import "./index.css";
+
 import App from "./App";
-import "antd/dist/antd.css";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./components/serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
